898bf344e11b8aae486247b61e271afd04d3c421
alessio nanni <alias@axew3.com>
2022-08-20 09:15:50 +0200

    Update common.php

--- w3all/displayonlyfirstpost/language/en/common.php
+++ w3all/displayonlyfirstpost/language/en/common.php
@@ -40,15 +40,13 @@ $lang = array_merge($lang, [
   //'DISPLAYONLYFIRSTPOST_REP_MODE_REPLACE'   => 'Replace the post text with custom content',
   //'DISPLAYONLYFIRSTPOST_SEP_BYCOMMA'    => ' &nbsp;if more than one, separate by comma',
 
-  //'DISPLAYONLYFIRSTPOST_EVENT'    => ' :: Displayonlyfirstpost Event :: ',
-
   'DISPLAYONLYFIRSTPOST_EVENT_REPLACEMENT_TEXT'   => '<p class="error">You can only view the first post into this topic.<br />Please register or join the memberships allowed to view all the replies!</p>',
 
   'ACP_DISPLAYONLYFIRSTPOST_GROUPS_IDS'     => 'Users Groups IDs that will see only the first post (separate by comma if more than one)',
   'ACP_DISPLAYONLYFIRSTPOST_FORUMS_IDS'     => 'ForumIDs of forums in which only the topic start post can be read.<br />If you wish to show only the first post in all forums at once, enter the word <i style="color:red"><b>all</b></i> as value',
-  'ACP_DISPLAYONLYFIRSTPOST_REP_MODE'     => 'Append custom content to the post text',
+  'ACP_DISPLAYONLYFIRSTPOST_REP_MODE'     => 'Prepend custom content to the post text',
+  'ACP_DISPLAYONLYFIRSTPOST_REP_CONTENT'     => 'Custom content that will be prepended to the post text',
   'ACP_DISPLAYONLYFIRSTPOST_SETTING_SAVED'  => 'Settings have been saved successfully!',
-  //'ACP_DISPLAYONLYFIRSTPOST_NOTES'  => 'Note that the extension is not active until both Users Groups and Forums (IDS) fields are not set<br />To change the text and html output for the <i style="font-size:85%">Replace the post text with custom content</i> option, see the file<br /><i style="font-size:85%">/ext/w3all/displayonlyfirstpost/language/_YourActiveLanguage_/<b>common.php</b></i>',
-  'ACP_DISPLAYONLYFIRSTPOST_NOTES'  => 'Note that the extension is not active until both Users Groups and Forums (IDS) fields are not set<br />To change the text and html output for the <i style="font-size:85%">text prepended into the first post</i>, see the file<br /><i style="font-size:85%">/ext/w3all/displayonlyfirstpost/language/_YourActiveLanguage_/<b>common.php</b></i>',
+  'ACP_DISPLAYONLYFIRSTPOST_NOTES'  => 'Note that the extension is not active until both Users Groups and Forums (IDS) fields are not set',
 
 ]);
